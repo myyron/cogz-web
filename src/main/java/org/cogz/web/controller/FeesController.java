a1ee72447596a46f17dd6759b67ea52ac48f6c7b
myyron <myyronmurray.latorilla@gmail.com>
2018-12-26 20:53:24 +0800

    added files for fee settings

---
+++ src/main/java/org/cogz/web/controller/FeesController.java
@@ -0,0 +1,20 @@
+package org.cogz.web.controller;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author mlatorilla
+ */
+@Controller
+@RequestMapping("/settings/fees")
+public class FeesController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showUserList(HttpSession session) {
+        return "settings/fees";
+    }
+}
